6fc5e348ad410be24a1a29c402f9c9e90f1eef06
Kumail <knanji123@gmail.com>
2019-08-08 18:30:21 -0400

    changes

--- src/pages/add.js
+++ src/pages/add.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './add.css';
+
+import juulTracker from '../img/JuulTracker.svg'
+import juul from '../img/Juul.svg'
+
+//Pods Import
+import mintPod from '../img/JuulCap.svg'
+import fruitPod from '../img/fruitPod.svg'
+import mangoPod from '../img/mangoPod.svg'
+import cucumberPod from '../img/cucumberPod.svg'
+import vanillaPod from '../img/vanillaPod.svg'
+import tobaccoPod from '../img/tobaccoPod.svg'
+
 
 function Add() {
-    return (
-        <div>
-        <h1>ADDDD MEEEEE</h1>
+  return (
+    <div className="container">
+
+      <div className="header">
+        <Link to="/">
+          <img src={juulTracker} className="juulLogo" />
+        </Link>
+      </div>
+
+      <div className="addNew">
+
+        <div className="pods">
+          <img src={mintPod} className="cap" />
+          <img src={fruitPod} className="cap" />
+          <img src={mangoPod} className="cap" />
+          <img src={cucumberPod} className="cap" />
+          <img src={vanillaPod} className="cap" />
+          <img src={tobaccoPod} className="cap" />
         </div>
-    );
-  }
+
+        <div className="percentage">
+          <button>1.5%</button>
+          <button>3%</button>
+          <button>5%</button>
+        </div>
+
+      </div>
+
+
+
+    </div>
+  );
+}
 
 export default Add;
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,27 +2,64 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './main.css';
 
+//Images
 import juulTracker from '../../img/JuulTracker.svg'
 import juul from '../../img/Juul.svg'
+import add from '../../img/add.svg'
+import juulCap from '../../img/JuulCap.svg'
 
-import myImage from '../logo.svg'
 
 
-const MainPage = () =>{
-    return (
-        <div className="container">
+const MainPage = () => {
+  return (
+    <div className="container">
 
-          <div className="header">
-            <img src={juulTracker} className="juulLogo"/>
+      <div className="header">
+        <img src={juulTracker} className="juulLogo" />
+      </div>
+
+      <div className="contents">
+        <div className="left">
+          <img src={juul} className="juul" />
+        </div>
+
+        <div className="right">
+          <h1>POD USAGE
+
+          <Link to="/add">
+            <img src={add} className="add" />
+          </Link>
+          </h1>
+
+          <div className="track">
+            <img src={juulCap} className="juulCap" />
+            <p className="title">5% - MINT</p>
+            <p className="time">July 29th, 2019<br></br>8:00am</p>
           </div>
 
-          <img src={juul} className="juul"/>
-        <h3>POD USAGE</h3>
-        <Link to="/add"> 
-          <img src={myImage} className="image" />
-        </Link>
+          <div className="track">
+            <img src={juulCap} className="juulCap" />
+            <p className="title">5% - MINT</p>
+            <p className="time">July 29th, 2019<br></br>8:00am</p>
+          </div>
+
+          <div className="track">
+            <img src={juulCap} className="juulCap" />
+            <p className="title">5% - MINT</p>
+            <p className="time">July 29th, 2019<br></br>8:00am</p>
+          </div>
+
+          <div className="track">
+            <img src={juulCap} className="juulCap" />
+            <p className="title">5% - MINT</p>
+            <p className="time">July 29th, 2019<br></br>8:00am</p>
+          </div>
+
         </div>
-    );
-  }
+
+      </div>
+    </div>
+  );
+}
 
 export default MainPage;
